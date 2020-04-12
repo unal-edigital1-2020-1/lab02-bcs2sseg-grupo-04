cca816ee0c3ba70e75698c6370fa7a9d7061afa2
jcrojasda <55933743+jcrojasda@users.noreply.github.com>
2020-04-12 16:43:21 -0500

    Update BCDtoSSeg.v

--- src/BCDtoSSeg.v
+++ src/BCDtoSSeg.v
@@ -1,14 +1,17 @@
-module BCDtoSSeg (BCD, SSeg, an);
+module BCDtoSSeg (BCD, SSeg, an); //Se crea el display de 7 segmentos y se declaran las variables de entrada y salida
 
   input [3:0] BCD;
   output reg [0:6] SSeg;
   output [3:0] an;
-
+	//Se define cada variable, si es de salida o entrada y el tamaño que va a tener
+	//BCD es el número que se va a mostrar en el display
+	//SSeg es para prender o apagar los LEDs del display
+	//an es el display a prender
 assign an=4'b1110;
+	//Se le asigna un valor a an, en este caso para prender el primer display
 
-
-always @ ( * ) begin
-  case (BCD)
+always @ ( * ) begin //siempre que haya un cambio ejecuta esta función
+	case (BCD) // para cada caso de BCD se le da un valor al SSeg para mostrar en el display
    4'b0000: SSeg = 7'b0000001; // "0"  
 	4'b0001: SSeg = 7'b1001111; // "1" 
 	4'b0010: SSeg = 7'b0010010; // "2" 
