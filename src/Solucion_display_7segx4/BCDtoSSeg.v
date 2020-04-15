8f80cbc3668f196e03c0262b13ca70630ea3c2f2
Sebastian Gil <jsgilr@unal.edu.co>
2020-04-14 19:33:30 -0500

    Update BCDtoSSeg.v

--- src/Solucion_display_7segx4/BCDtoSSeg.v
+++ src/Solucion_display_7segx4/BCDtoSSeg.v
@@ -16,12 +16,13 @@ always @ ( * ) begin
 	4'b0111: SSeg = 7'b0001111; // "7" 
 	4'b1000: SSeg = 7'b0000000; // "8"  
 	4'b1001: SSeg = 7'b0000100; // "9" 
-   4'ha: SSeg = 7'b0001000;  
-   4'hb: SSeg = 7'b1100000;
-   4'hc: SSeg = 7'b0110001;
-   4'hd: SSeg = 7'b1000010;
-   4'he: SSeg = 7'b0110000;
-   4'hf: SSeg = 7'b0111000;
+	  //La representacion del numero a mostrar debe ser de manera decimal por lo que se elimina esta parte del codigo
+   //4'ha: SSeg = 7'b0001000;  
+   //4'hb: SSeg = 7'b1100000;
+   //4'hc: SSeg = 7'b0110001;
+   //4'hd: SSeg = 7'b1000010;
+   //4'he: SSeg = 7'b0110000;
+   //4'hf: SSeg = 7'b0111000;
     default:
     SSeg = 0;
   endcase
